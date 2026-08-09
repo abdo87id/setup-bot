336a087b63128b4e5805154390654cfeb88e1f7f
abdo87id <abdlrhmanabdlrhmanabdlhfad@gmail.com>
2026-08-09 22:30:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -110,15 +110,15 @@ client.on("channelCreate", async (channel) => {
 // ===============================
 
 client.on("messageCreate", async (message) => {
-
   if (message.author.bot) return;
 
   if (message.content.includes("@everyone")) {
 
-    await message.channel.send(
-      "━━━━━━━━━━━━━━━━━━━━━━━━"
-    );
+    await message.channel.send({
+      files: ["./standard.gif"]
+    });
 
   }
 });
+
 client.login(TOKEN);
